37a7a010ad59fbe28d9acfc258077919fb109086
Honjes <samuel.papke@protonmail.com>
2021-07-29 13:45:23 +0200

    refactor: removing unused util functions

--- src/util/parse.ts
+++ src/util/parse.ts
@@ -39,46 +39,13 @@ export async function singeUpUser(userData: SingeUpUserData) {
 }
 
 /**
- * Function to request a User by its Username
- * @param value {string} - value of the User that should be given back
- * @param id {string} - value of the User that should be given back
- * @returns {Promise<any[]>} - the Array that the parse function returns
- */
-export async function requestUsers(
-  value: string,
-  id = "username"
-): Promise<any[]> {
-  const query = new Parse.Query(Parse.User)
-  query.equalTo(id, value)
-  return await query.find()
-}
-
-/**
- * Checks if The given Username is already taken
- * @param value {string} - value that should be checked on avalibility
- * @returns {Promise<boolean>} Returns if there is a User with the given value
- */
-export async function checkIfUsernameExists(value: string): Promise<boolean> {
-  //const response = await requestUsers(value, "username")
-  return false //response.length >= 1
-}
-
-/**
- * Checks if The given Email is already taken
- * @param email {string} - Email that should be checked on avalibility
- * @returns
- */
-export async function checkIfEmailExists(email: string): Promise<boolean> {
-  //const response = await requestUsers(email, "email")
-  return //response.length >= 1
-}
-
-/**
  * Checks if Parse is currently logged in and returns false if not
  * @returns {boolean} - Returns if there is a User set as boolean
  */
 export function getCurrentUser(): boolean {
-  const curUser = Parse.User.current()
-  console.log("curUser: ", curUser)
-  return Boolean(curUser)
+  if (!config.debug) {
+    const curUser = Parse.User.current()
+    console.log("curUser: ", curUser)
+    return Boolean(curUser)
+  } else return false
 }
