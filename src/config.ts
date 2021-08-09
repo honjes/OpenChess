3748db3b99b93d2e557728c17b1a6d4458f68644
Honjes <samuel.papke@protonmail.com>
2021-08-09 20:28:05 +0200

    feat: added interface for configObject

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
-export default {
+interface OCConfig {
+  debug: boolean
+  back4app_url: string
+  back4app_applicationId: string
+  back4app_javascriptKey: string
+  parse_secret: string
+  email_resend_delay_sec: number
+  [index: string]: string | number | boolean
+}
+
+const config: OCConfig = {
   debug: false, // Disables Accounts and all connections to the server
   back4app_url: "https://parseapi.back4app.com/",
   back4app_applicationId: "N1auF9Z0lGlmaLOkzKLCsnJbBpOPJvzroxfqTuRT",
   back4app_javascriptKey: "ZMmQSzeaetTcBLAy5F1UeTd6yYGu5EzNRU3fHnOY",
   parse_secret: "87zzbsdf/($%)=awdat5t51",
+  email_resend_delay_sec: 60,
 }
+
+export default config
